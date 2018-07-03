5fd8866501f78a53de9fee613ba7200da8b3f479
Brian Wilson <brianwilson@brians-macbook-pro.local>
2018-07-03 11:27:22 -0400

    Initial Commit

---
+++ autoLayout Baseball/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  autoLayout Baseball
+//
+//  Created by Brian Wilson on 7/2/18.
+//  Copyright © 2018 Brian Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+
+}
+
